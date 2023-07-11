7796dc9efe452e6ac51efeff7bd0b8c7cb315972
salkhaleeli <saad@Saads-MacBook-Pro.local>
2023-07-10 18:05:45 -0600

    changing componentto f

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,24 @@ import { useState } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
-class App extends Component {
-  // const { search, setSearch } = useState;
-  // const {newRobo, setNewRobo} = useState;
-  constructor(){
-    super()
-    this.state = {
-      robots: robots,
-      searchField: ''
-    }
+const App = () => {
+  const [search, setSearch] = useState('');
+
+  const onSearchChange = (e) => {
+    setSearch(e.target.value)
   }
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value })
-  }
-  render() {
-    const filteredRobots = this.state.robots.filter(robot =>{
-      return robot.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase())
-    })
-    return (
-      <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    )
-  }
+  const filtered = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(search.toLowerCase())
+  });
+
+  return (
+    <div className='tc'>
+      <h1>RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <CardList robots={filtered} />
+    </div>
+  )
 }
+
 
 export default App;
